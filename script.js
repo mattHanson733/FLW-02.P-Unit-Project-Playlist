2496d043134e9d25842fe8055a547fc8e56400b5
Matthew Hanson <108187519+mattHanson733@users.noreply.github.com>
2023-02-02 06:12:15 +0000

    Reworked entire project and added save playlist functionality

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
-// task 5: read through the JavaScript starter code to determine where each given function is declared and where each given function is called.
-function Song(imgURL, songName, artist, songURL) {
-  return {
-    imgURL: imgURL,
-    songName: songName,
-    artist: artist,
-    songURL: songURL
+"use strict";
+
+// Variables for storing user input
+const userImgURL = document.querySelector(".image");
+const userSongName = document.querySelector(".song-name");
+const userArtist = document.querySelector(".artist");
+const userSongURL = document.querySelector(".song-link");
+
+
+let playlistTable = document.querySelector('#playlistTable');
+
+const addSong = document.getElementById("addSong");
+
+const userPlaylist = [];
+
+
+class userSongData {
+  constructor(userImageURL, userSongName, userArtist, userSongURL) {
+    console.log('got to userSongData constructor');
+    this.userImageURL = userImageURL,
+      this.userSongName = userSongName,
+      this.userArtist = userArtist,
+      this.userSongURL = userSongURL
   }
 }
 
-// input variables
-let image = document.querySelector(".image");
-let songName = document.querySelector(".song-name");
-let artist = document.querySelector(".artist");
-let songLink = document.querySelector(".song-link");
+class Song {
+  #songNumber = 0;
 
-// button variable
-let add = document.querySelector(".add");
+  static numSongs = 0;
 
-// task 6: declare variables for your display divs: the image url, song name, artist, and song link. Go back to the HTML to check that you are using the correct class names.
-let display = document.querySelector('.display');
-let displayImage = document.querySelector(".display-image");
-let displaySong = document.querySelector(".display-song");
-let displayArtist = document.querySelector(".display-artist");
-let displayLink = document.querySelector(".display-link");
+  constructor(imgURL, songName, artist, songURL) {
+    this.songName = songName;
+    this.artist = artist;
+    this.imgURL = imgURL;
+    this.songURL = songURL;
 
+    Song.numSongs++;
+    this.#songNumber = Song.numSongs;
+    console.log(this.#songNumber);
+  }
 
+  get songNumber() {
+    return this.#songNumber;
+  }
+}
 
-// task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
-
-
-
-let defaultPlaylistItems = [
+const defaultPlaylistItems = [
   new Song(`https://i.ibb.co/CtP2kmn/KRS-One-album-Cover.jpg`, `MC’s Act Like They Don’t Know`, `KRS-One`, `https://www.youtube.com/watch?v=zTm0YulSONU`),
   new Song(`https://i.ibb.co/jHSSyY1/the-Low-End-Theory-album-Cover.jpg`, `Jazz(We’ve Got)`, `A Tribe Called Quest`, `https://youtube.com/watch?v=zlA0GHnERLs&feature=share`),
   new Song(`https://i.ibb.co/9psN6J9/a-Madmans-Dream-album-Cover.jpg`, `A Madman’s Dream (Dirty)`, `The Great East Flatbush Project`, `https://youtube.com/watch?v=6wzeG-iLQTg&feature=share`),
@@ -40,70 +55,175 @@ let defaultPlaylistItems = [
   new Song(`https://i.ibb.co/9tPC5mY/the-Infamous-album-Cover.jpg`, `Shook Ones, Pt. II`, `Mobb Deep`, `https://youtube.com/watch?v=SLjXz9ghUtk&feature=share`)
 ];
 
+class TableTools {
+
+  static createTD(numTD) {
+    let tdElements = [];
+
+    for (let i = 0; i < numTD; i++) {
+      let td = document.createElement("td");
+      tdElements.push(td);
+    }
+
+    return tdElements;
+  }
+
+  static createSongFragment(song) {
+
+    //console.log(`line 71: The songNumber of the current song is: ${song.getSongNumber()}`);
+    let tdElements = this.createTD(4);
+
+    let image = document.createElement("img");
+    image.setAttribute("src", song.imgURL);
+
+    let songLink = document.createElement("a");
+    songLink.setAttribute("href", song.songURL);
+    songLink.appendChild(document.createTextNode(song.songURL));
+
+    tdElements[0].appendChild(image);
+    tdElements[1].appendChild(document.createTextNode(song.songName));
+    tdElements[2].appendChild(document.createTextNode(song.artist));
+    tdElements[3].appendChild(songLink);
+
+    let songFragment = new DocumentFragment();
+
+    let tr = document.createElement("tr");
 
 
+    tr.setAttribute("id", `songNumber_${(song.songNumber).toString()}`);
+    songFragment.append(tr);
 
+    tdElements.map((tdElement) => {
+      songFragment.appendChild(tdElement);
+    });
 
-//REFACTOR ARRAYS DAY 
-// task 11: comment out the arrays data.
-// task 12: create an object for each of your songs.
-// task 13: inside each object, add key/value pairs to store the image url, song name, artist, and song link.
-// task 14: create an array that stores all of the objects.
+    return songFragment;
+  }
 
+  static appendSongFragmentToTable(songFragment, playlistTableElement) {
+    //console.log(`the type of displayTable is: ${typeof displayTable}`);
+    playlistTableElement.append(songFragment);
+  }
 
+  static emptyPlaylistTable() {
+    let table = document.querySelector(".playlistTable");
 
-//REFACTOR LOOPS DAY 
-// task 15: update your `addSongInfo` function so the input values are saved in as values in a new object.
-// task 16: update your `.push()` so the input object is added to your array of objects.
-// task 17: update your loops based on your new array of objects.
+    if (table === null) {
+      console.log("Error: No table element found");
+    } else {
+      table.textContent = "";
+    }
+  }
+}
 
+class PlaylistTools extends TableTools {
+  /*
+  Takes in the user's inputted song data as the userSongData type, 
+  instantiates a new Song object, and returns that Song object
+  */
+  static createSongFromUserData(userSongData) {
+    const { userImageURL, userSongName, userArtist, userSongURL } = userSongData;
 
+    let userSong = new Song(userImageURL, userSongName, userArtist, userSongURL);
 
+    //console.log(`This song's number is: ${userSong.songNumber}`)
+    return userSong;
+  }
 
+  /*
+    Creates a Song from the user's inputs (passed in through in a userSongData object) 
+    and renders it to the playlist table
+    */
+  static renderSong(userSong) {
+    let songFragment = this.createSongFragment(userSong);
 
-function addSongInfo() {
-  let userImgURL
-  let userSongName
-  let userArtist
-  let userSongURL = 
-  // task 9: declare a variable to save the user input of the image url. Declare three more variables that save user input: One for the song names, one for the artists, and a last one for the song links.
+    this.appendSongFragmentToTable(songFragment, playlistTable);
+  }
 
+  /*
+  Renders all of the items from the defaultPlaylistItems array to the playlist table on the page
+  
+  *Called on page load*
+  */
+  static initialPlaylistRender() {
 
-  // task 10: use `.push()` to add each input value to the correct array.
+    defaultPlaylistItems.map((song) => {
+      this.renderSong(song);
+    });
 
+    if (localStorage.length !== 0) {
+      let userPlaylist = JSON.parse(localStorage.getItem('userPlaylist'));
+
+      userPlaylist.map((userSongData) => {
+        let song = this.createSongFromUserData(userSongData);
+        this.renderSong(song);
+      });
+    }
+  }
+
+  //Include storeUserPlaylist function implementation here
+  static storeUserPlaylist(userPlaylist) {
+    if (userPlaylist.constructor === Array && userPlaylist.length > 0) {
+      localStorage.setItem('userPlaylist', JSON.stringify(userPlaylist));
+    } else {
+      console.log('Error (ln 159): storeUserPlaylist was not passed an array');
+    }
+  }
+
+  static deleteStoredUserPlaylist() {
+    localStorage.removeItem('userPlaylist');
+  }
+}
+
+class InputValidators {
+  static checkForInvalidInputs(object) {
+    let invalidInput = false;
+
+    Object.values(object).map((value) => {
+      if (value === undefined || value === null || value === '') {
+        invalidInput = true;
+      }
+    });
+
+    return invalidInput;
+  }
 }
 
 
+function handleAddSongClick() {
+  let userInputs = [
+    userImgURL.value,
+    userSongName.value,
+    userArtist.value,
+    userSongURL.value
+  ]
 
+  let userSongInfo = new userSongData(...userInputs);
 
-/******** this function empties the display divs each time the button is clicked so that your playlist does not repeatedly add the data too many times. Where should this function be placed???********/
-function emptyDisplay() {
-  displayImage.innerHTML = "";
-  displaySong.innerHTML = "";
-  displayArtist.innerHTML = "";
-  displayLink.innerHTML = "";
+  let invalidInputs = InputValidators.checkForInvalidInputs(userSongInfo);
+  //console.log(userSongInfo);
+
+  //console.log(new Song (...userSongInfo));
+  if (invalidInputs !== true) {
+    PlaylistTools.renderSong(
+      // Creates a new Song object from a userSongData object, pushes it to the user playlist, and returns the song object as the argument to its parent renderSong function
+      function() {
+        let userSong = PlaylistTools.createSongFromUserData(userSongInfo)
+        userPlaylist.push(userSong);
+        return userSong;
+      }()
+    );
+  }
+}
+
+function handleSavePlaylistClick() {
+  PlaylistTools.deleteStoredUserPlaylist();
+
+  PlaylistTools.storeUserPlaylist(userPlaylist);
 }
 
 
-
-
-function displaySongInfo() {
-
-  // task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
-
-
-
-}
-
-
-
-
-
-// click event to add and display songs
-add.onclick = function() {
-  addSongInfo();
-  displaySongInfo();
-};
-
-// function call to display stored songs
-displaySongInfo();
+const handleDeletePlaylistClick = () => { PlaylistTools.deleteStoredUserPlaylist(); }
+  
+// Render the default playlist items to the page
+PlaylistTools.initialPlaylistRender();
